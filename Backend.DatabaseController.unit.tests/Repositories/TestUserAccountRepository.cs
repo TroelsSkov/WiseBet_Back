f0d826611e142a25da1cce8766b53d8cb13d847c
Aske Johansen <202405193@post.au.dk>
2026-03-16 21:37:37 +0100

    Fixed errors and added Testfile for UserRepo

---
+++ Backend.DatabaseController.unit.tests/Repositories/TestUserAccountRepository.cs
@@ -0,0 +1,19 @@
+﻿using WiseBet.backend.IRepository;
+using WiseBet.backend.Data;
+using WiseBet.backend.DTOs;
+namespace Backend.DatabaseController.unit.tests.Repositories;
+public class Tests
+{
+    private UserAccountRepository m_uut;
+    [SetUp]
+    public void Setup()
+    {
+        m_uut = new UserAccountRepository(new DatabaseContext());
+    }
+
+    [Test]
+    public void CheckTestsAreWorkingPassedAlways()
+    {
+        Assert.Pass();
+    }
+}
