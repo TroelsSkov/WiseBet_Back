6a0d7b51be4ec33f26f8cde47bbe658a1b1b1f8f
Aske Johansen <202405193@post.au.dk>
2026-03-17 18:08:00 +0100

    Fixed issue where foreign keys were not seen in Models/Chats.cs

--- Backend.Controllers_Database/Migrations/20260317170628_UserAccountForeignKeyFix.Designer.cs
+++ Backend.Controllers_Database/Migrations/20260317170628_UserAccountForeignKeyFix.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WiseBet.backend.Data;
 
@@ -11,9 +12,11 @@ using WiseBet.backend.Data;
 namespace WiseBet.backend.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20260317170628_UserAccountForeignKeyFix")]
+    partial class UserAccountForeignKeyFix
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Backend.Controllers_Database/Migrations/20260317170628_UserAccountForeignKeyFix.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WiseBet.backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserAccountForeignKeyFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Chats_UserAccounts_UserAccountUserID",
+                table: "Chats");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Chats_UserAccountUserID",
+                table: "Chats");
+
+            migrationBuilder.DropColumn(
+                name: "UserAccountUserID",
+                table: "Chats");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Chats_UserID",
+                table: "Chats",
+                column: "UserID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Chats_UserAccounts_UserID",
+                table: "Chats",
+                column: "UserID",
+                principalTable: "UserAccounts",
+                principalColumn: "UserID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Chats_UserAccounts_UserID",
+                table: "Chats");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Chats_UserID",
+                table: "Chats");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserAccountUserID",
+                table: "Chats",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Chats_UserAccountUserID",
+                table: "Chats",
+                column: "UserAccountUserID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Chats_UserAccounts_UserAccountUserID",
+                table: "Chats",
+                column: "UserAccountUserID",
+                principalTable: "UserAccounts",
+                principalColumn: "UserID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
