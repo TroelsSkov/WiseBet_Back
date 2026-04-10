32207bfde112ba81f9bd408f10d447badadffa17
Troels Skov <troelsskov8@gmail.com>
2026-04-10 14:30:22 +0200

    Oprettede Identity user til security og securitymodul

---
+++ Backend.Controllers_Database/Configs/AddSecurityService.cs
@@ -0,0 +1,9 @@
+namespace WiseBet.backend.Configs;
+
+public static class SecurityServiceConfiguration
+{
+    public static IServiceProvider AddCustomSecurityService(IServiceCollection services)
+    {
+        
+    }    
+}
