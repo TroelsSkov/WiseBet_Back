3012ce6405ca6eb63c25d298c67d62c474ceae45
MortenAgger <mortenlaumannagger@gmail.com>
2026-04-30 12:26:51 +0200

    Added two roulette DTO's

--- Backend.Controllers_Database/Services/DTOs/RouletteDto.cs
+++ Backend.Controllers_Database/Services/DTOs/RouletteDto.cs
@@ -3,5 +3,16 @@ namespace WiseBet.backend.Services.DTOs;
 
 public class RouletteDTO
 {
+    public Guid SessionId {get; set;}
+    public RouletteSessionStatus Status{get; set;}
 
+    public int ActiveUsers {get; set;}
+    public int MaxUsers {get; set;} = 5; 
+
+    public int SecondsLeft {get; set;}
+
+    public int? WinningNumber { get; set; }
+    
+    public RouletteBetType? WinningColor { get; set; }
+    public List<Guid> Participants { get; set; } = new();
 }
